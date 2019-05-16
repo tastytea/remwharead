835ef81620c147b0b36aa77ac0f371ebb5b2302d
tastytea <tastytea@tastytea.de>
2019-05-17 01:50:31 +0200

    Added tests for CSV export.

---
+++ tests/test_csv.cpp
@@ -0,0 +1,72 @@
+/*  This file is part of remwharead.
+ *  Copyright © 2019 tastytea <tastytea@tastytea.de>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 3.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <exception>
+#include <string>
+#include <chrono>
+#include <sstream>
+#include <catch.hpp>
+#include "time.hpp"
+#include "sqlite.hpp"
+#include "export.hpp"
+
+using std::string;
+using std::chrono::system_clock;
+
+SCENARIO ("The CSV export works correctly")
+{
+    bool exception = false;
+    bool csv_ok = false;
+
+    GIVEN ("One database entry")
+    {
+        Database::entry entry;
+        entry.uri = "https://example.com/page.html";
+        entry.tags = { "tag1", "tag2" };
+        entry.title = "Nice title";
+        entry.datetime = system_clock::time_point();
+        entry.fulltext = "Full text.";
+        entry.description = "Good description.";
+
+        try
+        {
+            std::ostringstream output;
+            export_csv({ entry }, output);
+
+            const string expected =
+                "\"URI\",\"Archived URI\",\"Date & time\",\"Tags\","
+                "\"Title\",\"Description\",\"Full text\"\r\n"
+                "\"https://example.com/page.html\",\"\","
+                "\"1970-01-01T01:00:00\",\"tag1,tag2\","
+                "\"Nice title\",\"Good description.\",\"Full text.\"\r\n";
+            if (output.str() == expected)
+            {
+                csv_ok = true;
+            }
+        }
+        catch (const std::exception &e)
+        {
+            exception = true;
+        }
+
+        THEN ("No exception is thrown")
+            AND_THEN ("Output looks okay")
+        {
+            REQUIRE_FALSE(exception);
+            REQUIRE(csv_ok);
+        }
+    }
+}
