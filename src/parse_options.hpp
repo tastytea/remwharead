3f3964973a3c20873048dbecdd3917dc39576f1d
tastytea <tastytea@tastytea.de>
2019-05-17 00:30:03 +0200

    Fixed array initialisation for gcc-5.

--- src/parse_options.hpp
+++ src/parse_options.hpp
@@ -37,7 +37,7 @@ typedef struct options
     vector<string> tags;
     export_format format = export_format::undefined;
     string file;
-    array<time_point, 2> span = { time_point(), system_clock::now() };
+    array<time_point, 2> span = {{ time_point(), system_clock::now() }};
     string uri;
     uint8_t status_code = 0;
 
