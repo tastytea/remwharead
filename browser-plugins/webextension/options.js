42fbf0a55dc02b7e1360a3078ca80ef2b7179a58
tastytea <tastytea@tastytea.de>
2019-05-24 13:43:15 +0200

    WebExtension: Added shortcut.

--- browser-plugins/webextension/options.js
+++ browser-plugins/webextension/options.js
@@ -16,5 +16,25 @@ function restore_options()
               });
 }
 
+function info_commands(commands)
+{
+    commands.forEach(
+        function(command)
+        {
+            var element = document.querySelector("#shortcuts");
+            var para = document.createElement("p");
+
+            var text = document.createElement("strong");
+            text.appendChild(document.createTextNode(command.shortcut + ": "));
+            para.appendChild(text);
+            para.appendChild(document.createTextNode(command.description));
+
+            element.appendChild(para);
+        });
+}
+
 document.addEventListener('DOMContentLoaded', restore_options);
 document.querySelector("#archive").addEventListener("change", save_options);
+
+var get_commands = browser.commands.getAll();
+get_commands.then(info_commands);
