1d802d5b679a8d7de05d0baee68e05755244a5e2
tastytea <tastytea@tastytea.de>
2019-07-26 01:24:12 +0200

    Added tests for Unicode handling in to_lowercase().

---
+++ tests/test_unicode.cpp
@@ -0,0 +1,33 @@
+/*  This file is part of remwharead.
+ *  Copyright © 2019 tastytea <tastytea@tastytea.de>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 3.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <catch.hpp>
+#include "search.hpp"
+
+
+SCENARIO ("Unicode is handled correctly")
+{
+    WHEN ("In to_lowercase()")
+    {
+        THEN ("Results look okay")
+        {
+            REQUIRE(to_lowercase("SUPPENGRÜN") == "suppengrün");
+            REQUIRE(to_lowercase("SCHEIẞE") == "scheiße");
+            REQUIRE(to_lowercase("ШОКОЛАД") == "шоколад");
+        }
+    }
+}
+
