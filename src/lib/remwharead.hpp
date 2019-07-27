27a470e21fbcd8633f7aeaea2f60ea43d74522f8
tastytea <tastytea@tastytea.de>
2019-07-27 23:18:34 +0200

    Added remwharead.hpp, which includes all other headers.
    It also contains the main page for the reference.

---
+++ src/lib/remwharead.hpp
@@ -0,0 +1,44 @@
+/*  This file is part of remwharead.
+ *  Copyright © 2019 tastytea <tastytea@tastytea.de>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 3.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef REMWHAREAD_HPP
+#define REMWHAREAD_HPP
+
+/*!
+ *  @mainpage remwharead Reference
+ *
+ *  @section using Using the library
+ *
+ *  The easiest way is to include remwharead.hpp, which then includes all other
+ *  headers.
+ *
+ *  @code
+ *  #include <remwharead/remwharead.hpp>
+ *  @endcode
+ */
+
+#include "export/adoc.hpp"
+#include "export/bookmarks.hpp"
+#include "export/csv.hpp"
+#include "export/export.hpp"
+#include "export/simple.hpp"
+#include "search.hpp"
+#include "sqlite.hpp"
+#include "time.hpp"
+#include "types.hpp"
+#include "uri.hpp"
+
+#endif  // REMWHAREAD_HPP
