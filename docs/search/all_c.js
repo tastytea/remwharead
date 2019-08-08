0bc0bbdf8accff8256073d360d2949458955b7d0
tastytea <tastytea@tastytea.de>
2019-08-08 22:13:06 +0200

    2019-08-08

--- docs/search/all_c.js
+++ docs/search/all_c.js
@@ -1,10 +1,10 @@
 var searchData=
 [
-  ['search',['Search',['../classremwharead_1_1Search.html',1,'remwharead::Search'],['../classremwharead_1_1Search.html#a1f3b35d44c51253318ba47df2d26a9ca',1,'remwharead::Search::Search()']]],
-  ['search_5fall',['search_all',['../classremwharead_1_1Search.html#afee3d1f4614461d4b8a0f1f99464bcb3',1,'remwharead::Search']]],
-  ['search_5ftags',['search_tags',['../classremwharead_1_1Search.html#aafbaaed1c49b13b891b724c045fefaeb',1,'remwharead::Search']]],
+  ['search',['Search',['../classremwharead_1_1Search.html',1,'remwharead::Search'],['../classremwharead_1_1Search.html#ad7f2772675d501d882d7004480e27ba3',1,'remwharead::Search::Search()']]],
+  ['search_5fall',['search_all',['../classremwharead_1_1Search.html#a5f30a5f0e729148e27e2493b208988e4',1,'remwharead::Search']]],
+  ['search_5ftags',['search_tags',['../classremwharead_1_1Search.html#a7f62ba62afb751f19ca9d690d4ead1f0',1,'remwharead::Search']]],
   ['simple',['Simple',['../classremwharead_1_1Export_1_1Simple.html',1,'remwharead::Export']]],
-  ['sort_5fentries',['sort_entries',['../classremwharead_1_1Export_1_1ExportBase.html#a0a0515408c0af2f0d74fe8e483882384',1,'remwharead::Export::ExportBase']]],
+  ['sort_5fentries',['sort_entries',['../classremwharead_1_1Export_1_1ExportBase.html#ac2e5bc57ab62fac930b02ad3a7c9003e',1,'remwharead::Export::ExportBase']]],
   ['store',['store',['../classremwharead_1_1Database.html#ab3ea9062c5c0ef5d9f2b7eea6f0fd1bd',1,'remwharead::Database']]],
   ['strip_5fhtml',['strip_html',['../classremwharead_1_1URI.html#ad6ad5351ecf2983e01f9f4a51c2057a5',1,'remwharead::URI']]]
 ];
--- docs/search/functions_8.js
+++ docs/search/functions_8.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['remove_5fhtml_5ftags',['remove_html_tags',['../classremwharead_1_1URI.html#a8b340b13ccf0bc3ae9059872ce48e06a',1,'remwharead::URI']]],
   ['remove_5fnewlines',['remove_newlines',['../classremwharead_1_1URI.html#a9373cb28de198ae2db624980273ece4a',1,'remwharead::URI']]],
-  ['retrieve',['retrieve',['../classremwharead_1_1Database.html#abc9c9adf58b786342b0492baeef8f4ec',1,'remwharead::Database']]]
+  ['retrieve',['retrieve',['../classremwharead_1_1Database.html#ab08e7bd3ef03257f7ed951ef051db4fd',1,'remwharead::Database']]]
 ];
